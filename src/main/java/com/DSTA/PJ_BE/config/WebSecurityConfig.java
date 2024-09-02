75910eb5e61e5fc886fb37d97fd5489f84d1b7c4
Midoraa <binnguyen301002@gmail.com>
2024-09-02 14:59:29 +0700

    done api getNewestBlogs, getaBlogById, getBlogsByTopic

--- src/main/java/com/DSTA/PJ_BE/config/WebSecurityConfig.java
+++ src/main/java/com/DSTA/PJ_BE/config/WebSecurityConfig.java
@@ -90,6 +90,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/api/account/sendMail/{email}").permitAll()
 		.antMatchers("/api/account/verifyOtp/{otp}").permitAll()
 		.antMatchers("/api/account/createNewPass/{newPass}").permitAll()
+		.antMatchers("/api/blog/**").permitAll()
         .anyRequest().authenticated()
         .and()
 		.logout()
